82b6f45d5976cde0ce2afdeae212b7e1afbc8e77
Samuel <40303843+sasdallas@users.noreply.github.com>
2022-04-21 11:01:41 -0500

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,31 +60,19 @@ const Hero = () => {
 
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Samuel S.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const three = <h3 className="big-heading">I create software.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
+        I’m a young software engineer specializing in programming complicated software.
+        Currently, I'm just beginning to learn more high-level languages. I've created
+        some interesting software, and am trying to create more!
       </p>
     </>
   );
-  const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
-    </a>
-  );
+  
 
-  const items = [one, two, three, four, five];
+  const items = [one, two, three, four];
 
   return (
     <StyledHeroSection>
